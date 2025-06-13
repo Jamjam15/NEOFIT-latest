0f3a77b9f2df97d40c5554d131c11138267a27c0
ken <cajayonkenth30@gmail.com>
2025-06-13 18:34:52 +0800

    return , cancel, aproved or rejects and message to admin

--- Admin Pages/manage_order_details_page.php
+++ Admin Pages/manage_order_details_page.php
@@ -632,12 +632,9 @@ $stats_result = $conn->query($stats_sql)->fetch_assoc();
                     <i class="fas fa-inbox"></i>
                     <span>Inbox</span>
                 </li>                <li class="active">
+                </li>                <li class="active">
                     <i class="fas fa-list"></i>
                     <span>Manage Orders</span>
-                </li>
-                <li onclick="window.location.href='manage_returns.php'">
-                    <i class="fas fa-undo"></i>
-                    <span>Returns</span>
                 </li>
                 <li onclick="window.location.href='manage_returns.php'">
                     <i class="fas fa-undo"></i>
